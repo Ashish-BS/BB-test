c1b86563f60ba3b66a29134e3a00f86f72586f17
Ashish04Kumar <ashishishere11@gmail.com>
2023-10-04 18:41:16 +0530

    changes in stats-card

--- src/components/common/social-proof-chart.tsx
+++ src/components/common/social-proof-chart.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Card from './card';
-import socialStatsJson from '@/fixtures/social-stats.json'
-
+import React from "react";
+import Card from "./card";
+import socialStatsJson from "@/fixtures/social-stats.json";
 
 export function SocialProofChart() {
-    return (
-        <div className='row'>
-            {socialStatsJson && socialStatsJson.map(item => (
-                <div className="col-lg-4 col-md-6 col-sm-12 my-2" key={item.id}>
-                    <Card {...item} />
-                </div>
-            ))}
-        </div>
-    )
+  return (
+    <div className="row">
+      {socialStatsJson &&
+        socialStatsJson.map((item) => (
+          <div className="col-lg-4 col-md-6 col-sm-12 my-2" key={item.id}>
+            <Card {...item} />
+          </div>
+        ))}
+    </div>
+  );
 }
